96ea608261c81d76dec6232dd475639d070f2875
cespirit <ckespir@gmail.com>
2014-12-03 16:58:41 -0800

    Refactor code

--- js/app.js
+++ js/app.js
@@ -1,24 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {	
+	var introPlaying = false;
+	var player = $("#ryu");
 	playIntro();
 
-	$(".ryu-container").mouseenter(function(){
-		$(".ryu-cool").hide();
-		$(".ryu-still").hide();
-		$(".ryu-throwing").hide();
-		$(".ryu-ready").show();
+	player.mouseenter(function(){
+		changeAction(player, "ready");
 	})
 	.mouseleave(function(){
-		$(".ryu-cool").hide();
-		$(".ryu-ready").hide();
-		$(".ryu-throwing").hide();
-		$(".ryu-still").show();		
+		changeAction(player, "still");
 	})
 	.mousedown(function(){
-		playHadouken();
-		$(".ryu-cool").hide();
-		$(".ryu-ready").hide();
-		$(".ryu-still").hide();			
-		$(".ryu-throwing").show();	
+		playMusic($("#hadouken-sound")[0], 0.5, false, true)
+		changeAction(player, "throwing");
 		$(".hadouken").finish().show()
 		.animate(
 		    {"left": "300px"},
@@ -31,56 +24,51 @@ $(document).ready(function(){
 		);		
 	})
 	.mouseup(function(){
-		$(".ryu-cool").hide();
-		$(".ryu-ready").hide();
-		$(".ryu-throwing").hide();
-		$(".ryu-still").show();
+		changeAction(player, "still");
 	});
-	
+
 	$(document).keydown(function(event){
 		if(event.keyCode === 88) {		
-			playCool();
-			$(".ryu-ready").hide();
-			$(".ryu-still").hide();
-			$(".ryu-throwing").hide();
-			$(".ryu-cool").show();
+			playMusic($("#cool-sound")[0], 0.5, true, false)
+			changeAction(player, "cool-pose");
 		}
-	}).keyup(function(event){
+	})
+	.keyup(function(event){
 		if(event.keyCode === 88) {
-			stopCool();
-			$(".ryu-cool").hide();
-			$(".ryu-ready").hide();
-			$(".ryu-throwing").hide();
-			$(".ryu-still").show();		
+			stopMusic($("#cool-sound")[0]);
+			changeAction(player, "still");	
 		}
 	});
 
 });
 
 function playIntro(){
-	$("#sf-theme")[0].volume = 0.5;
-	$("#sf-theme")[0].play();
-
+	introPlaying = true;
+	playMusic($("#sf-theme")[0], 0.5, false, false);
 	$(".sf-logo").fadeIn(1200).delay(1000).fadeOut(1000);
-	$(".brought-by").delay(3200).fadeIn(1200).delay(1000).fadeOut(1000);
-	$(".jquery-logo").delay(6400).fadeIn(1200).delay(1000).fadeOut(1000);
-	$(".instructions").delay(9600).fadeIn(1200);
+	$(".brought-by").delay(3300).fadeIn(1200).delay(1000).fadeOut(1000);
+	$(".jquery-logo").delay(6600).fadeIn(1200).delay(1000).fadeOut(1000);
+	$(".instructions").delay(9900).fadeIn(1200);
 }
 
-function playHadouken() {
-	$("#hadouken-sound")[0].volume = 0.5;
-	$("#hadouken-sound")[0].load();
-	$("#hadouken-sound")[0].play();
+function changeAction(player, action) {
+	player.removeClass();
+	player.addClass(player[0].id + '-' + action);
 }
 
-function playCool(){
-	$("#sf-theme")[0].pause();
-	$("#cool-sound")[0].volume = 0.5;	
-	$("#cool-sound")[0].play();
+function playMusic(audio, vol, pauseIntro, reload) {
+	if(introPlaying && pauseIntro) { 
+		$("#sf-theme")[0].pause(); 
+		introPlaying = false;
+	}
+	if(reload) { 
+		audio.load(); 
+	}
+	audio.volume = vol;
+	audio.play();
 }
 
-function stopCool() {
-	$("#cool-sound")[0].pause();
-	$("#cool-sound")[0].load();
+function stopMusic(audio) {
+	audio.pause();
+	audio.load(); 
 }
-
